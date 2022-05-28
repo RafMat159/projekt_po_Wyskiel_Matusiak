7745891ab72df1be73816f69a0f6d021f71af383
RafMat159 <rmatusiak159@gmail.com>
2022-05-28 20:04:20 +0200

    first step class Grafika

--- src/com/company/Grafik.java
+++ src/com/company/Grafik.java
@@ -2,6 +2,14 @@ package com.company;
 
 public class Grafik {
 
-    
+    private String[][] tygodniowySzablon = new String[4][8]; //3 - mozliwe zmiany w ciagu dnia + wiersz na dzien tygodnia,
+                                                            // 7 - liczba dni tygodnia + wiersz na godzine
+
+    private class Ranking{          //klasa wewnetrzna
+      private String[][] statystyki;
+        public Ranking(int liczbaPracownikow) {
+            statystyki = new String[liczbaPracownikow][3];
+        }
+    }
 
 }
