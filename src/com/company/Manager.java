bae50615c118430e0b4a755f4faa277c7af9fdc8
RafMat159 <rmatusiak159@gmail.com>
2022-06-01 11:44:48 +0200

    update of the liczbaPrzepracowanychGodzin field

--- src/com/company/Manager.java
+++ src/com/company/Manager.java
@@ -5,22 +5,22 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Manager extends Osoba {
-    private List<String> listaPracownikow = new ArrayList<>();
+    private List<Pracownik> listaPracownikow = new ArrayList<>(); //KOMPOZYCJA
 
     public Manager(int idPracownika, double tygWyplataBrutto, double tygWyplataNetto, double liczbaPrzepracowanychGodzin,
                    double stawkaGodzinowa, String imie, String nazwisko, String status) {
         super(idPracownika, tygWyplataBrutto, tygWyplataNetto, liczbaPrzepracowanychGodzin, stawkaGodzinowa, imie, nazwisko, status);
     }
 
-    public void dodajPracownika(String imie, String nazwisko, int idPodwladnego){
-        listaPracownikow.add(imie + " " + nazwisko + ", " + idPodwladnego);
+    public void dodajPracownika(Pracownik pracownik){
+        listaPracownikow.add(pracownik);
     }
 
     public void ustalGrafik(Grafik grafik){
         Scanner in = new Scanner(System.in);
         System.out.println("Pracownicy: ");//Wyswietlenie listy pracownikow
         for(int i = 0; i < listaPracownikow.size(); i++){
-            System.out.println(listaPracownikow.get(i));
+            System.out.println(listaPracownikow.get(i).IdImieNazwiskoPracownika());
         }
         for(int i = 1; i <= 7; i++) {
             for (int j = 1; j <= 3; j++){
@@ -43,21 +43,24 @@ public class Manager extends Osoba {
                     System.out.println("Nie istnieje pracownik o takim ID!");
                     j--;
                 }
-                else
+                else {
                     grafik.uzupelnijGrafik(i, j, Integer.toString(id));
+                    int index = znajdzId(id);
+                    listaPracownikow.get(index).setLiczbaPrzepracowanychGodzin(listaPracownikow.get(index).getLiczbaPrzepracowanychGodzin()+8.0); //zwiekszanie liczby przepracowanych godzin
+                }
             }
         }
     }
 
     public void zmodyfikujGrafik(Grafik grafik){
-        System.out.println("Wybierz dzień do edycji:\n1.Poniedzialek\n2.Wtorek\n3.Sroda\n4.Czwartek\n5.Piatek\n6.Sosbota\n7.Niedziela");
+        System.out.println("Wybierz dzień do edycji:\n1.Poniedzialek\n2.Wtorek\n3.Sroda\n4.Czwartek\n5.Piatek\n6.Sobota\n7.Niedziela");
         Scanner in = new Scanner(System.in);
         int dzien = in.nextInt();
         System.out.println("Wybierz zmianę:\n1.6.00-14.00\n2.14.00-22.00\n3.22.00-6.00");
         int zmiana = in.nextInt();
         System.out.println("Wybierz pracownika");
         for(int i = 0; i < listaPracownikow.size(); i++){
-            System.out.println(listaPracownikow.get(i));
+            System.out.println(listaPracownikow.get(i).IdImieNazwiskoPracownika());
         }
         int id = in.nextInt();
         boolean prawda = true;
@@ -66,10 +69,24 @@ public class Manager extends Osoba {
                 System.out.println("Nie istnieje pracownik o takim ID!\nWybierz pracownika o poprawnym id:");
                 id = in.nextInt();
             }
-            else
+            else {
                 prawda = false;
+                int index = znajdzId(id);
+                String[][] tabGrafik = grafik.getTygodniowySzablon();
+                int indexStarego = znajdzId(Integer.parseInt(tabGrafik[zmiana][dzien]));
+                listaPracownikow.get(indexStarego).setLiczbaPrzepracowanychGodzin(listaPracownikow.get(indexStarego).getLiczbaPrzepracowanychGodzin()-8.0); //zmniejszenie liczby przepracowanych godzin
+                listaPracownikow.get(index).setLiczbaPrzepracowanychGodzin(listaPracownikow.get(index).getLiczbaPrzepracowanychGodzin()+8.0); //zwiekszanie liczby przepracowanych godzin
+            }
         }
         grafik.uzupelnijGrafik(dzien, zmiana, Integer.toString(id));
+    }
+
+    private int znajdzId(int idPracownika){ //odszukanie idPracownika dla ktorego zmieniona zostanie liczba przepracowanych godzin
+        int i = 0;
+        while(idPracownika != listaPracownikow.get(i).getIdPracownika()){
+            i++;
+        }
+        return i;
     }
 
     @Override
--- src/com/company/Pracownik.java
+++ src/com/company/Pracownik.java
@@ -10,7 +10,12 @@ public class Pracownik extends Osoba {
         super(idPracownika, tygWyplataBrutto, tygWyplataNetto, liczbaPrzepracowanychGodzin, stawkaGodzinowa, imie, nazwisko, status);
         this.imieManagera = imieManagera;
         this.nazwiskoManagera = nazwiskoManagera;
-        manager.dodajPracownika(imie,nazwisko,idPracownika); //dodanie pracownikow do danego managera
+        manager.dodajPracownika(this); //dodanie pracownikow do danego managera
+    }
+
+
+    public String IdImieNazwiskoPracownika(){
+        return getImie() + " " + getNazwisko() + ", " + getIdPracownika();
     }
 
     public void SprawdzWyplate(){
@@ -20,9 +25,6 @@ public class Pracownik extends Osoba {
         System.out.println("Tygodniowa wyplata brutto wynosi: " + wyplataTygB + "\nTygodniowa wyplata netto wynosi: " + wyplataTygN);
 
     }
-    //pasuje jakos ogarnac liczbe przepracowanych godzin danego pracownika, a dokladnie w jakim momencie ja zmieniac, czy w momencie wpisywania osoby do grafiku zmieniac
-    //wartosc tej zmiennej i pozniej podczas modyfikacji grafiku przez managera aktualizowac te zmienna, czy w momencie sprawdzania przez pracownika swojego grafiku(chociaz
-    // to jest chyba troche gorsze rozwiazanie)
 
     //Premia,SprawdzSwojGrafik - pracownik bedzie korzystal z funkcji ktora zostala napisana w klasie Osoba,SprawdzWyplate,PodniesStawke
 }
